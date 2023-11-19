6e101d702c8282d4f73ef5cc85912480fccce9e7
hooleygun <korplyakov00@mail.ru>
2023-11-20 00:21:37 +0300

    Slider fixes 2.1

--- www/scripts/slider.js
+++ www/scripts/slider.js
@@ -10,8 +10,7 @@ function rooms_base(){
         roomsId.push(rooms[i].id);
     };
 
-    var currentRoomId = "room-index-1";
-    showRoom(currentRoomId);
+    currentRoom("room-index-1")
 }
 
 function currentRoom(n){
@@ -19,7 +18,6 @@ function currentRoom(n){
 }
 
 function showRoom(m){
-
     for(i=0;i<rooms.length;i++){
         if (i+1 == Number(m.slice(-1))){
             rooms[i].style.display = "block";
@@ -32,11 +30,28 @@ function showRoom(m){
     currentSlide(m,1);
 }
 
+function plusSlides(roomId, leap){
+    for(i=0;i<slides.length;i++){
+        if (slides[i].id=="slide-index-"+roomId){
+            if (slides[i].style.display == "block"){
+                slides[i].style.display = "none";
+                slides[i+leap].style.display = "block";
+                return;
+            }
+        }
+        if (i+leap>=slides.length){
+            slides[0].style.display = "block";
+            slides[i].style.display = "none";
+            return;
+        }
+    }
+}
+
 function currentSlide(r_index, s_index){
     showSlide(r_index, s_index);
 }
 
-function showSlide(room, currentSlide){
+function showSlide(room, slide){
     var slidesId = [];
     for(j=0;j<slides.length;j++){
         slides[j].style.display = "none";
@@ -51,7 +66,7 @@ function showSlide(room, currentSlide){
     };
 
     for (j=0;j<slidesId.length;j++){
-        if (j==currentSlide-1){
+        if (j==slide-1){
             slidesId[j].style.display = "block";
         }
     };
