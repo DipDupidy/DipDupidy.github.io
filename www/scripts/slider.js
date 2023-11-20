e3ab720d728c0d8b3594f576a0d13c3cefffc592
Egor Korplyakov <korplyakov00@mail.ru>
2023-11-20 15:40:24 +0300

    Slider 1.0

--- www/scripts/slider.js
+++ www/scripts/slider.js
@@ -1,6 +1,7 @@
 var rooms = document.getElementsByClassName("room-info-block");
 var roomsId = [];
 var slides = document.getElementsByClassName("img-slides");
+var dots = document.getElementsByClassName("counter-dot");
 
 rooms_base();
 
@@ -32,30 +33,39 @@ function showRoom(m){
 
 function plusSlides(roomId, leap){
     let slidesBase = [];
+    var dotsId = [];
     for(i=0;i<slides.length;i++){
         if (slides[i].id=="slide-index-"+roomId){
             slidesBase[i] = slides[i];
+            dotsId.push(dots[i]);
         }
     };
 
     let filteredSlidesBase = slidesBase.filter(Object);
-    
+
+    for (i=0;i<dots.length;i++){
+        dots[i].className = dots[i].className.replace("active","");
+    }
+
     for (i=0;i<filteredSlidesBase.length;i++){
         if (filteredSlidesBase[i].style.display == "block" & i+leap>=filteredSlidesBase.length){
             filteredSlidesBase[0].style.display = "block";
             filteredSlidesBase[i].style.display = "none";
+            dotsId[0].className += " active";
             return;
         };
         if (filteredSlidesBase[i].style.display == "block" & i==0 & i+leap<0){
             filteredSlidesBase[filteredSlidesBase.length-1].style.display = "block";
             filteredSlidesBase[i].style.display = "none";
+            dotsId[filteredSlidesBase.length-1].className += " active";
             return;
         };
         if (filteredSlidesBase[i].style.display == "block"){
             filteredSlidesBase[i].style.display = "none";
             filteredSlidesBase[i+leap].style.display = "block";
+            dotsId[i+leap].className += " active";
             return;
-        }
+        };
     };
 }
 
@@ -65,6 +75,7 @@ function currentSlide(r_index, s_index){
 
 function showSlide(room, slide){
     var slidesId = [];
+    var dotsId = [];
     for(j=0;j<slides.length;j++){
         slides[j].style.display = "none";
     };
@@ -74,12 +85,18 @@ function showSlide(room, slide){
         let roomId = room;
         if (slideId.slice(-1)==roomId.slice(-1)){
             slidesId.push(slides[j]);
+            dotsId.push(dots[j]);
+            dots[j].className = dots[j].className.replace("active","");
         }
     };
 
+    //document.getElementsByClassName("counter-dot active") = null;
+
+    //dots[j].className += " default";
     for (j=0;j<slidesId.length;j++){
         if (j==slide-1){
             slidesId[j].style.display = "block";
+            dotsId[j].className += " active";
         }
     };
 }
